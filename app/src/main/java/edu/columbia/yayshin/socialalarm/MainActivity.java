2336a9edcfcb770e411ce18979ff60a8f505a7c1
yayshine <ys2657@columbia.edu>
2017-04-04 23:41:04 -0400

    Rename MyAlarmActivity -> MainActivity

--- app/src/main/java/edu/columbia/yayshin/socialalarm/MainActivity.java
+++ app/src/main/java/edu/columbia/yayshin/socialalarm/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MyAlarmsActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -35,7 +35,7 @@ public class MyAlarmsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_alarms);
+        setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
